664fcb3c99ca8ac326b62ae0698be55f5edc2bf2
Harrison Massey <harrison.massey@gmail.com>
2013-11-06 20:08:18 -0600

    Adjusted to use TCP

--- index.js
+++ index.js
@@ -1,39 +1,24 @@
 var Midiplex = require('midiplex')
-var es = require('event-stream')
-var dgram = require('dgram')
 var midi = require('midi')
 var through = require('through')
+var net = require('net')
 
-function udpSocket() { return new dgram.createSocket('udp4') }
-
-function udpStream(socket) {
-  return es.readable(function(count, callback) {
-    var self = this
-    socket.on('message', function(buf){
-      self.emit('data', buf)
-      callback()
-    })
-  })
-}
 var mp = new Midiplex()
 
-var modSocketX = udpSocket()
-var velSocket = udpSocket()
-var modSocketY = udpSocket()
+function controlServer(controlNum) {
+  return net.createServer(function(stream) {
+    mp.addControllerStream(stream, {maxVal: 127, controller: controlNum})
+  })
+}
 
-var modXStream = udpStream(modSocketX)
-var velStream = udpStream(velSocket)
-var modYStream = udpStream(modSocketY)
+var serverX = controlServer(1)
+var serverY = controlServer(2)
 
-mp.addControllerStream(modXStream, {maxVal: 127, controller: 1})
-//mp.setVelocityStream(velStream, {maxVal: 200})
-mp.addControllerStream(modYStream, {maxVal: 127, controller: 2})
-
-modSocketX.bind(9001)
-modSocketY.bind(9002)
+serverX.listen(9001)
+serverY.listen(9002)
 
 var output = new midi.output()
-console.log(output.getPortName(1))
-output.openPort(1)
+console.log(output.getPortName(0))
+output.openPort(0)
 var midiStream = midi.createWriteStream(output)
 mp.getReadableStream().pipe(midiStream)
